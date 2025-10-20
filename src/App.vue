7c33839796b13b74b9393a09aa3d515f8240a702
carpediem <aas9429@naver.com>
2025-10-21 03:45:06 +0900

    vue 애니메이션 효과 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <NavBar />
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -9,3 +13,15 @@
 import NavBar from './components/NavBar.vue'
 export default { name: 'App', components: { NavBar } }
 </script>
+
+<style>
+/* 페이지 전환 애니메이션 */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
